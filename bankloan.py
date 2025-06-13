75443bb45f1f69b6ec8365f07e8f72bfa50d1806
Sandra <sandrachepkoech60@gmail.com>
2025-06-13 10:46:30 +0300

    Add files via upload

---
+++ bankloan.py
@@ -0,0 +1,7 @@
+
+age = int(input("enter age:"))
+income = float(input("Enter your income"))
+if (age > 21 and income > 21000):
+    print( "you qualify for  loan")    
+else:
+    print("you dont qualify for a loan" )
